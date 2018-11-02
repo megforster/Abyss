5b8c733440f443ad346d8cc338499f11d7352a28
Megan Forster <meganforster@Megans-MacBook-Pro.local>
2018-11-02 14:05:54 -0400

    Beginning of commenting

--- src/mainGame/EnvironmentalObstacle.java
+++ src/mainGame/EnvironmentalObstacle.java
@@ -12,69 +12,56 @@ import javax.imageio.ImageIO;
 
 public class EnvironmentalObstacle extends GameObject {
 
-	private Handler handler;
-	private int sizeX;
-	private int sizeY;
-	private Player player;
+	//Instance Variables
+	private int sizeX; // length of hitbox
+	private int sizeY; // height of hitbox
 	private Image img;
-	private static Random r = new Random();
-	private double x, y;
+	private static Random r = new Random(); // random number generator used in move()
+	private double x, y; // x and y coordinate on screen
 
+	//Constructor
 	public EnvironmentalObstacle(double x, double y, ID id, Handler handler, Player player) {
 		super(x, y, id);
-		this.handler = handler;
-		this.player = player;
-		this.velX = 0;
-		this.velY = 0;
-		this.x = x;
-		this.y = y;
+		this.x = x; //sets x coordinate to random number generated in Spawn10to20 or Spawn1-10
+		this.y = y; //sets x coordinate to random number generated in Spawn10to20 or Spawn1-10
 
-		
-				try {
-					img = ImageIO.read(new File("images/EnvironmentalObstacle.png"));
-					img = img.getScaledInstance(200, 200, 0);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				sizeX = img.getWidth(null);
-				sizeY = img.getHeight(null);
-		getBounds();	
+		try {
+			img = ImageIO.read(new File("images/EnvironmentalObstacle.png")); // reads image for obstacle visual
+			img = img.getScaledInstance(200, 200, 0); // scales image down
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		sizeX = img.getWidth(null); // set hit box width to image width
+		sizeY = img.getHeight(null); // sets hit box height to image height
+		getBounds(); // generates the hit box
 	}
 
 	@Override
+	//Needed so class can extend GameObject
 	public void tick() {
-
 	}
 
 	@Override
+	//Draws the obstacle 
 	public void render(Graphics g) {
-		double centerX = x + img.getWidth(null) / 2;
-		double centerY = y +  img.getHeight(null) / 2;
-		
-		double angle = -Math.atan2(velX, velY);
-		      
-		
-		
-		     
-        Graphics2D g2d = (Graphics2D)g; // Create a Java2D version of g.
-        AffineTransform reset = new AffineTransform();
-        reset.rotate(0, 0, 0);
-        Graphics2D g2 = (Graphics2D)g;
-        g2.rotate((angle), centerX, centerY);
-        g2d.drawImage(img, (int) this.x, (int) this.y, null);
-        g2.setTransform(reset);
+		Graphics2D g2d = (Graphics2D) g; // Create a Java2D version of g.
+		AffineTransform reset = new AffineTransform();
+		reset.rotate(0, 0, 0);
+		Graphics2D g2 = (Graphics2D) g;
+		g2d.drawImage(img, (int) this.x, (int) this.y, null);
+		g2.setTransform(reset);
 	}
 
-
-	
+	//Creates the hit box
 	public Rectangle getBounds() {
-		return new Rectangle((int) this.x+30, (int) this.y+30, this.sizeX-60, this.sizeY-60);
-	
+		return new Rectangle((int) this.x + 30, (int) this.y + 30, this.sizeX - 60, this.sizeY - 60);
+
 	}
-	
+
+	//Generates the new random location when player runs into obstacle
 	public void move() {
-	 this.x = r.nextInt(1000)*1;
-	 this.y = r.nextInt(500)*1;
+		this.x = r.nextInt(1000) * 1;
+		this.y = r.nextInt(500) * 1;
 	}
 
 }
--- src/mainGame/Player.java
+++ src/mainGame/Player.java
@@ -25,6 +25,7 @@ import mainGame.Game.STATE;
 
 public class Player extends GameObject {
 
+	// Instance variables
 	Random r = new Random();
 	Handler handler;
 	private HUD hud;
@@ -35,85 +36,95 @@ public class Player extends GameObject {
 	public static int playerSpeed = 14;
 	private EnvironmentalObstacle obstacle;
 
+	// Constructor
 	public Player(double x, double y, ID id, Handler handler, HUD hud, Game game) {
 		super(x, y, id);
 		this.handler = handler;
 		this.hud = hud;
 		this.game = game;
 		this.damage = 4;
-		playerWidth = 32;
-		playerHeight = 32;
-		
+
 		img = null;
 		try {
-			img = ImageIO.read(new File("images/PlayerSprite.png"));
+			img = ImageIO.read(new File("images/PlayerSprite.png")); // reads image for obstacle visual
+			img = img.getScaledInstance(50, 50, 0); // scales image down
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		playerWidth = img.getWidth(null); // sets hit box width to image width
+		playerHeight = img.getHeight(null); // sets hit box height to image height
 	}
 
 	@Override
+	/*
+	 * Updates player location based on velocity, checks if the player has collided
+	 * with an enemy or obstacle, checks if the player is dead
+	 */
 	public void tick() {
 		this.x += velX;
 		this.y += velY;
+		// makes sure player cannot go outside screen bounds
 		x = Game.clamp(x, 0, Game.WIDTH - 38);
 		y = Game.clamp(y, 0, Game.HEIGHT - 60);
 
-
 		collision();
 		checkIfDead();
-		//drawFirstBullet(); //uncomment this line to regain shooting with movement
+		// drawFirstBullet(); //uncomment this line to regain shooting with movement
 
 	}
-	
 
+	// Checks if the player is dead
 	public void checkIfDead() {
-		if (hud.health <= 0) {// player is dead, game over!
+		if (hud.health <= 0) {// checks if player has a health level at or below 0
 
-			if (hud.getExtraLives() == 0) {
-				game.gameState = STATE.GameOver;
+			if (hud.getExtraLives() == 0) { // checks if the player has an extra life
+				game.gameState = STATE.GameOver; // changes game state to game over screen
 			}
 
-			else if (hud.getExtraLives() > 0) {// has an extra life, game continues
-				hud.setExtraLives(hud.getExtraLives() - 1);
-				hud.setHealth(100);
+			else if (hud.getExtraLives() > 0) {// checks if the player has an extra life
+				hud.setExtraLives(hud.getExtraLives() - 1); // Decreases player's amount of extra lives
+				hud.setHealth(100); // sets player health to full health
 			}
 		}
 	}
 
-	/**
-	 * Checks for collisions with all of the enemies, and handles it accordingly
-	 */
+	// Checks if player collides with enemies or obstacle
 	public void collision() {
 
-		hud.updateScoreColor(Color.white);
-		for (int i = 0; i < handler.object.size(); i++) {
-			GameObject tempObject = handler.object.get(i);
+		hud.updateScoreColor(Color.white); // changes Hud color on to visually display impact
+		for (int i = 0; i < handler.object.size(); i++) { // cycles through handler objects
+			GameObject tempObject = handler.object.get(i); // sets a temporary object to the object type handler is on
 
+			// checks if tempObject is an enemy or envrionemtnal obstacle
 			if (tempObject.getId() == ID.EnemyBasic || tempObject.getId() == ID.EnemyFast
 					|| tempObject.getId() == ID.EnemySmart || tempObject.getId() == ID.EnemyBossBullet
 					|| tempObject.getId() == ID.EnemySweep || tempObject.getId() == ID.EnemyShooterBullet
 					|| tempObject.getId() == ID.EnemyBurst || tempObject.getId() == ID.EnemyShooter
-					|| tempObject.getId() == ID.BossEye||tempObject.getId()==ID.EnvironmentalObstacle) {// tempObject is an enemy
+					|| tempObject.getId() == ID.BossEye || tempObject.getId() == ID.EnvironmentalObstacle) {
 
-				// collision code
-				if (getBounds().intersects(tempObject.getBounds())) {// player hit an enemy
-					if(tempObject.getId()==ID.EnvironmentalObstacle) {
-						obstacle = (EnvironmentalObstacle)tempObject;
+				// Checks if a collision has occured with the temporary object
+				if (getBounds().intersects(tempObject.getBounds())) {
+					// code of collision with obstacle
+					if (tempObject.getId() == ID.EnvironmentalObstacle) {
+						obstacle = (EnvironmentalObstacle) tempObject;
 						obstacle.move();
-						hud.health-=2;
+						hud.health -= 2;
 						hud.updateScoreColor(Color.red);
-						
-					}else {
-					hud.health -= damage;
-					hud.updateScoreColor(Color.red);
+						// code for collision with enemy
+					} else {
+						hud.health -= damage;
+						hud.updateScoreColor(Color.red);
 					}
 				}
 
 			}
-			if (tempObject.getId() == ID.EnemyBoss) {
-				// Allows player time to get out of upper area where they will get hurt once the
-				// boss starts moving
+			if (tempObject.getId() == ID.EnemyBoss) { // checks if tempObject is a boss
+				/*
+				 * Allows player time to get out of upper area where they will get hurt once the
+				 * boss starts moving
+				 */
 				if (this.y <= 300 && tempObject.isMoving) {
 					hud.health -= .5;
 					hud.updateScoreColor(Color.red);
@@ -123,66 +134,61 @@ public class Player extends GameObject {
 		}
 	}
 
-
+	// Draws the player sprite
+	//Comment this further 
 	public void render(Graphics g) {
-	
-		double centerX = x + playerWidth / 2;
-		double centerY = y +  playerHeight / 2;
-		
-		double angle = -Math.atan2(velX, velY);
-		      
-		
-		
-		     
-        Graphics2D g2d = (Graphics2D)g; // Create a Java2D version of g.
-        AffineTransform reset = new AffineTransform();
-        reset.rotate(0, 0, 0);
-        Graphics2D g2 = (Graphics2D)g;
-        g2.rotate((angle), centerX, centerY);
 
-        g2d.drawImage(img, (int) this.x, (int) this.y, playerWidth, playerHeight, null);
-        g2.setTransform(reset);
+		double centerX = x + playerWidth / 2;
+		double centerY = y + playerHeight / 2;
+
+		double angle = -Math.atan2(velX, velY);
+
+		Graphics2D g2d = (Graphics2D) g; 
+		AffineTransform reset = new AffineTransform();
+		reset.rotate(0, 0, 0);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.rotate((angle), centerX, centerY);
+
+		g2d.drawImage(img, (int) this.x, (int) this.y, playerWidth, playerHeight, null);
+		g2.setTransform(reset);
 	}
 
 	@Override
+	//creates player hit box
 	public Rectangle getBounds() {
 		return new Rectangle((int) this.x, (int) this.y, 32, 32);
 	}
 
+	//sets damage player receives 
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
 
+	//sets player width and height
+	//comment this better
 	public void setPlayerSize(int size) {
 		this.playerWidth = size;
 		this.playerHeight = size;
 	}
 
-	/*public void drawFirstBullet() {
-		if (velX != 0 || velY != 0) 
-			handler.addObject(new PlayerBullet((int) this.x + 8, (int) this.y + 5, velX, velY, ID.PlayerBullet, handler));
-	}
-	*/
-	
-
-	
+	//returns the player x location
 	public double getX() {
 		return this.x;
 	}
-	
+
+	//returns the player y location
 	public double getY() {
 		return this.y;
 	}
-	
+
+	//returns the player y velocity 
 	public double getVelY() {
 		return this.velY;
 	}
-	
+
+	//returns the player x velocity
 	public double getVelX() {
 		return this.velX;
 	}
-	
-	
 
 }
-//so sneak+2
